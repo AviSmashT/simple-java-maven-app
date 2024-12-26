dafb5b4bd8b63bcaa26567811e978e890883455f
Avi Shem Tov <60136061+AviSmashT@users.noreply.github.com>
2024-12-27 01:55:32 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,29 +20,29 @@ resource "aws_instance" "deployment-maven-actions" {
     Name = "deployment-maven-actions" # the name of the instance
   }
     
-    user_data = <<-EOF
+  user_data = <<-EOF
     #!/bin/bash
-    
+        
     # Add Docker's official GPG key:
     sudo apt-get update
     sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
-    
+        
     # Add the repository to Apt sources:
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-
+    
     # Start docker service and add ubuntu to the docker group:
     sudo service docker start
     sudo usermod -a -G docker ubuntu
-    	
-   	docker pull avishemtov2/maven-actions:latest 
-   	docker run -d avishemtov2/maven-actions:latest
+        	
+    docker pull avishemtov2/maven-actions:latest 
+    docker run -d avishemtov2/maven-actions:latest
   EOF
   
 }
