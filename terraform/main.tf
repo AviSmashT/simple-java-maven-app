098a12ddd5e087e20e3b594256d48c9d98f540cd
Avi Shem Tov <60136061+AviSmashT@users.noreply.github.com>
2024-12-27 02:21:15 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "deployment-maven-actions" {
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update -y
 
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Start docker service and add ubuntu to the docker group:
     sudo service docker start
